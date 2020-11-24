67727b29f8fab5f3fbcaaddc2db6e7c5eb7e8236
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-11-24 15:23:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+//https://teachablemachine.withgoogle.com/models/wSWeak6Pi/
 function preload(){}
 
 function setup(){
     canvas=createCanvas(400,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wSWeak6Pi/model.json",modelloaded);
+}
+function modelloaded(){
+    console.log("Modal Loaded");
+}
+
+function draw(){
+    image(video,0,0,400,300);
+    classifier.classify(video,getresult);
+}
+function getresult(error,results) {
+ if(error){
+     console.error(error);
+ }else{
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML=results[0].label;
+     document.getElementById("result_accuracy_name").innerHTML=results[0].confidence.toFixed(3)*100+"%";
+ }
 }
